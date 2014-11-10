7276f09bb87b908f077a4b8e961bdcb7eecb3b57
WojciechFocus <blachowiczwojciech@gmail.com>
2014-11-10 15:10:08 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,63 @@
+__author__ = 'wojciech'
+
+from Logic import *
+from visualize import *
+
+
+def build_magazine(width, height):
+  '''
+    builds simple magazin. There are no exit points, nor shelfs. 
+    These are to be implemented in the future
+  '''
+    magazine = Magazine(width, height, [])
+    magazine.getTile(0, 0).setDir(Direction(up=True))
+    magazine.getTile(width-1, 0).setDir(Direction(left=True))
+    magazine.getTile(0, height-1).setDir(Direction(right=True))
+    magazine.getTile(width-1, height-1).setDir(Direction(down=True))
+    for i in range(1, width-1):
+        magazine.getTile(i, 0).setDir(Direction(True, False, False, True))
+        magazine.getTile(i, height-1).setDir(Direction(right=True))
+
+    for i in range(1, height-1):
+        magazine.getTile(0, i).setDir(Direction(up=True, right=True))
+        magazine.getTile(width-1, i).setDir(Direction(down=True, left=True))
+        for j in range(1, width-1, 3):
+            magazine.getTile(j, i).setDir(Direction(left=True))
+        for j in range(2, width-1, 3):
+            magazine.getTile(j, i).setDir(Direction(right=True))
+        for j in range(3, width-1, 3):
+            magazine.getTile(j, i).setDir(Direction(up=True, left=True, right=True))
+    return magazine
+
+
+def visualize_test():
+    pos = Position(1, 1)
+    robot = Robot(pos, Direction())
+    robot_list = []
+    robot_list.append(robot)
+    magazine = build_magazine(15, 15)
+    magazine.addRobot(robot)
+    animation = RobotVisualization(len(magazine.getRobots()),
+                                   magazine.getWidth(),
+                                   magazine.getHeight())
+    for i in range(200):
+
+        for robot in magazine.getRobots():
+            robot.setDir(magazine.getTile(
+                robot.getPos().getX(), robot.getPos().getY()).getDir())
+            if robot.getDir().getUp():
+                robot.setPos(Position(robot.getPos().getX(), robot.getPos().getY() + 1))
+            elif robot.getDir().getRight():
+                robot.setPos(Position(robot.getPos().getX() + 1, robot.getPos().getY()))
+            elif robot.getDir().getDown():
+                robot.setPos(Position(robot.getPos().getX(), robot.getPos().getY() - 1))
+            elif robot.getDir().getLeft():
+                robot.setPos(Position(robot.getPos().getX() - 1, robot.getPos().getY()))
+            print robot.getDir()
+            print robot.getPos()
+        animation.update(magazine, magazine.getRobots())
+    animation.done()
+
+
+visualize_test()
+#buildMagazin(15,15)
