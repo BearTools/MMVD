2c2e13240f269a351336da60a5377ca7a8245c7d
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2014-12-14 00:55:59 +0100

    Little PEP8 changes

--- mmvdApp/main.py
+++ mmvdApp/main.py
@@ -6,6 +6,7 @@
 # 3 load order
 
 import click
+from .utils import read_warehouse_map, read_robots_positions
 
 _click_path = click.Path(exists=True, dir_okay=False, readable=True,
                          resolve_path=False)
@@ -16,9 +17,15 @@ _click_path = click.Path(exists=True, dir_okay=False, readable=True,
 @click.argument("robots", type=_click_path)
 @click.argument("order", type=_click_path)
 def main(warehouse, robots, order):
-    print click.format_filename(warehouse),
-    print click.format_filename(robots)
-    print click.format_filename(order)
+    """
+    Start application and:
+    - load specified warehouse map
+    - load initial robots positions
+    - load specific order
+    """
+    map = read_warehouse_map(click.format_filename(warehouse))
+    robots = read_robots_positions(click.format_filename(robots))
+    # order = click.format_filename(order)
 
 
 if __name__ == "__main__":
